a1e96d0b1a25b2648d4bce2eee41c5e77c1779cc
Charlie Sloan <CharliePESloan@gmail.com>
2019-01-06 17:34:35 +0000

    Edited function edit-functions

--- Bash/edit-functions.sh
+++ Bash/edit-functions.sh
@@ -1,11 +1,15 @@
 function nf()
 {
-	f=$1.sh;
+	FILE=$1.sh;
 	cd ~/MyBash/Bash/;
-	vim $f;
-	git add $f;
-	git commit -m "Edited function $1";
-	git push -u origin master;
+	vim $FILE;
+	if [ ! -f $FILE ]; then
+		git add $f;
+		git commit -m "Edited function $1";
+		git push -u origin master;
+		exec bash;
+	fi
+	cd -;
 }
 
 export -f nf
